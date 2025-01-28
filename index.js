bb5c223b52890ab1103d1b81d7daf75e96aafe58
Muhammet Çağrı Yılmaz <yilmaz004@outlook.com>
2025-01-29 00:28:17 +0300

    second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // TheMealDB API Base URL
 const BASE_URL = "https://www.themealdb.com/api/json/v1/1";
